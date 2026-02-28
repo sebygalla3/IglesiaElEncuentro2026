f22cd74d27cceee865b992ba66ff5e019d4add40
sebygalla3 <sebygalla3@gmail.com>
2026-02-28 00:34:08 -0300

    truncate

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,10 +10,10 @@ const Card = ({
 }) => {
   return (
     <div
-      className={`${bgClass} rounded-lg p-10 shadow-md ${className}`}
+      className={`${bgClass} rounded-lg p-10 shadow-md  ${className}`}
       {...rest}
     >
-      {title && <h2 className="text-5xl font-bold mb-6">{title}</h2>}
+      {title && <h2 className="text-4xl font-bold mb-6 truncate">{title}</h2>}
       {subtitle && <h3 className="text-3xl font-bold text-gray-600 mb-4">{subtitle}</h3>}
       {description && <p className="text-gray-700 text-xl">{description}</p>}
     </div>
