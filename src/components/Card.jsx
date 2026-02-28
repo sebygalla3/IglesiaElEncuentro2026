605681702c150138ea8b53d9f12d4b90805e70d6
sebygalla3 <sebygalla3@gmail.com>
2026-02-28 00:47:52 -0300

    titulo card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,12 +10,12 @@ const Card = ({
 }) => {
   return (
     <div
-      className={`${bgClass} rounded-lg p-10 shadow-md  ${className}`}
+      className={`${bgClass}  rounded-lg p-7 shadow-md  ${className}`}
       {...rest}
     >
-      {title && <h2 className="text-4xl font-bold mb-6 truncate">{title}</h2>}
+      {title && <h2 className="text-6xl font-bold mb-2 truncate">{title}</h2>}
       {subtitle && <h3 className="text-3xl font-bold text-gray-600 mb-4">{subtitle}</h3>}
-      {description && <p className="text-gray-700 text-xl">{description}</p>}
+      {description && <p className="text-gray-700 text-base text-justify">{description}</p>}
     </div>
   );
 };
